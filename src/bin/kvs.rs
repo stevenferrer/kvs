7f14c2500fe539c5ef61b1e759cf586fbb9fede1
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-08-20 20:06:06 +0800

    Cargo fmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -4,50 +4,50 @@ use clap::{Args, Parser, Subcommand};
 #[clap(author, version, about, long_about = None)]
 #[clap(propagate_version = true)]
 struct Cli {
-  #[clap(subcommand)]
-  command: Commands,
+    #[clap(subcommand)]
+    command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-  Set(SetArgs),
-  Get(GetArgs),
-  Rm(RmArgs),
+    Set(SetArgs),
+    Get(GetArgs),
+    Rm(RmArgs),
 }
 
 #[derive(Args)]
 struct SetArgs {
-  #[clap(value_parser)]
-  key: String,
+    #[clap(value_parser)]
+    key: String,
 
-  #[clap(value_parser)]
-  value: String,
+    #[clap(value_parser)]
+    value: String,
 }
 
 #[derive(Args)]
 struct GetArgs {
-  #[clap(value_parser)]
-  key: String,
+    #[clap(value_parser)]
+    key: String,
 }
 
 #[derive(Args)]
 struct RmArgs {
-  #[clap(value_parser)]
-  key: String,
+    #[clap(value_parser)]
+    key: String,
 }
 
 fn main() {
-  let cli = Cli::parse();
+    let cli = Cli::parse();
 
-  match &cli.command {
-    Commands::Set(_) => {
-      panic!("unimplemented")
+    match &cli.command {
+        Commands::Set(_) => {
+            panic!("unimplemented")
+        }
+        Commands::Get(_) => {
+            panic!("unimplemented")
+        }
+        Commands::Rm(_) => {
+            panic!("unimplemented")
+        }
     }
-    Commands::Get(_) => {
-      panic!("unimplemented")
-    }
-    Commands::Rm(_) => {
-      panic!("unimplemented")
-    }
-  }
 }
--- src/kv.rs
+++ src/kv.rs
@@ -2,25 +2,25 @@ use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct KvStore {
-  map: HashMap<String, String>,
+    map: HashMap<String, String>,
 }
 
 impl KvStore {
-  pub fn new() -> KvStore {
-    KvStore {
-      map: HashMap::new(),
+    pub fn new() -> KvStore {
+        KvStore {
+            map: HashMap::new(),
+        }
     }
-  }
 
-  pub fn set(&mut self, k: String, v: String) {
-    self.map.insert(k, v);
-  }
+    pub fn set(&mut self, k: String, v: String) {
+        self.map.insert(k, v);
+    }
 
-  pub fn get(&mut self, k: String) -> Option<String> {
-    self.map.get(&k).map(|v| v.to_string())
-  }
+    pub fn get(&mut self, k: String) -> Option<String> {
+        self.map.get(&k).map(|v| v.to_string())
+    }
 
-  pub fn remove(&mut self, k: String) {
-    self.map.remove(&k);
-  }
+    pub fn remove(&mut self, k: String) {
+        self.map.remove(&k);
+    }
 }
